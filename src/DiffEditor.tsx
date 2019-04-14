03a9bcc0721f9632fc7003a9a40b8c02cc665421
DoSun <chenxuan958864951@qq.com>
2019-04-14 19:59:33 +0800

    [UPDATE] 去掉 react-monaco-editor 依赖，自己实现编辑器组件

--- src/DiffEditor.tsx
+++ src/DiffEditor.tsx
@@ -1,49 +1,74 @@
+import * as monacoApi from "monaco-editor/esm/vs/editor/editor.api";
 import * as React from "react";
-import { MonacoDiffEditor } from "react-monaco-editor";
-import "./DiffEditor.css";
+
 import { diff, file, origin } from "./file";
 import { PlugIn as diffEditorPlugIn } from "./plugins/computeDiff";
 
-const editorOptions = {};
+import "./DiffEditor.css";
 
 interface IState {
-  fileContent: string;
-  originalContent: string;
+  renderWhitespace: "boundary" | "none";
 }
 
 export class DiffEditor extends React.Component<{}, IState> {
-  public editor: any;
-  public navi: any;
+  public editorElement: HTMLDivElement;
+  public editor: monacoApi.editor.IStandaloneDiffEditor;
+  public navi: monacoApi.editor.IDiffNavigator | null;
 
   constructor(props: any) {
     super(props);
+
     this.state = {
-      fileContent: "",
-      originalContent: ""
+      renderWhitespace: "boundary"
     };
   }
 
-  public editorDidMount = (editor: any, monaco: any) => {
-    this.editor = editor;
-    this.navi = monaco.editor.createDiffNavigator(this.editor, {
-      followsCaret: true, // resets the navigator state when the user selects something in the editor
-      ignoreCharChanges: true // jump from line to line
-    });
-    diffEditorPlugIn(this.editor);
+  public assignRef = (component: HTMLDivElement) => {
+    this.editorElement = component;
   };
 
+  public componentDidMount = () => {
+    this.initMonaco();
+  };
+
+  public componentWillUnmount = () => {
+    if (this.editor) {
+      this.editor.dispose();
+    }
+  };
+
+  public initMonaco = () => {
+    if (this.editorElement) {
+      this.editor = monacoApi.editor.createDiffEditor(this.editorElement, {
+        readOnly: true,
+        renderWhitespace: "boundary"
+      });
+      this.navi = monacoApi.editor.createDiffNavigator(this.editor, {
+        followsCaret: true, // resets the navigator state when the user selects something in the editor
+        ignoreCharChanges: true // jump from line to line
+      });
+      monacoApi.editor.setTheme("vs-dark");
+
+      diffEditorPlugIn(this.editor);
+    }
+  };
+
+  public updateModel(value: string, original: string, language: string) {
+    const originalModel = monacoApi.editor.createModel(original, language);
+    const modifiedModel = monacoApi.editor.createModel(value, language);
+    this.editor.setModel({
+      modified: modifiedModel,
+      original: originalModel
+    });
+  }
+
   public handleLoadBtn = () => {
-    // tslint:disable-next-line: no-console
-    this.originalEditor.setValue(origin);
-    this.modifiedEditor.setValue(file);
-    this.editor.setDiff(diff);
+    this.updateModel(file, origin, "scss");
+    (this.editor as any).setDiff(diff);
   };
 
   public handleGetBtn = () => {
-    // tslint:disable-next-line:no-console
-    console.log(this.editor._diffComputationResult);
-    // tslint:disable-next-line:no-console
-    console.log(diff);
+    console.log("test");
   };
 
   public handleNaviPrev = () => {
@@ -58,6 +83,13 @@ export class DiffEditor extends React.Component<{}, IState> {
     }
   };
 
+  public handleToggleWhiteSpace = () => {
+    const renderWhitespace =
+      this.state.renderWhitespace === "none" ? "boundary" : "none";
+    this.editor.updateOptions({ renderWhitespace });
+    this.setState({ renderWhitespace });
+  };
+
   get originalEditor() {
     return this.editor ? this.editor.getOriginalEditor() : null;
   }
@@ -67,22 +99,24 @@ export class DiffEditor extends React.Component<{}, IState> {
   }
 
   public render() {
-    const { fileContent, originalContent } = this.state;
     return (
       <div className="container">
         <div className="toolbar">
           <button onClick={this.handleLoadBtn}>load</button>
           <button onClick={this.handleGetBtn}>get</button>
+          <button onClick={this.handleToggleWhiteSpace}>
+            toggle whitespace
+          </button>
           <button onClick={this.handleNaviPrev}>上一个</button>
           <button onClick={this.handleNaviNext}>下一个</button>
         </div>
-        <MonacoDiffEditor
-          height="calc(100% - 50px)"
-          language="css"
-          original={originalContent}
-          value={fileContent}
-          options={editorOptions}
-          editorDidMount={this.editorDidMount}
+        <div
+          ref={this.assignRef}
+          style={{
+            height: "calc(100% - 50px)",
+            width: "100%"
+          }}
+          className="monaco-editor-container"
         />
       </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// load monaco
+import "monaco-editor/esm/vs/editor/editor.main";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
